fcc3a73c8e868a0d30fec5b9e4c70f0b75c069b8
mickeyleetw <mickeyleetw23@gmail.com>
2020-12-22 09:45:08 +0800

    201222_bugFixed

--- public/script/neweditor.js
+++ public/script/neweditor.js
@@ -1,67 +1,20 @@
-// cache dom
-var $shareBtn = $('.share-btn');
-var $shareUrl = $('.share-url');
-var $shareContainer = $('.share-container');
-var $notificationButton = $('.notification-button');
-
-// set data
-var $url = document.location.href;
-var $shared = false;
-
-/**
- *
- */
-function shareLink(e){
-
-    // set active class
-    $shareBtn.toggleClass('active');
-    $shareUrl.toggleClass('active');
-    $shareContainer.toggleClass('active');
-
-    if ($shared === false) {
-
-        // trigger notification alert
-        $notificationButton.toggleClass('active');
-        $shared = true;
-        $shareBtn.text('link');
-        $shareUrl.text($url);
-
-        var range = document.createRange();
-        range.selectNode($(this).next()[0]);
-        window.getSelection().addRange(range);
-
-        try {
-            // Now that we've selected the anchor text, execute the copy command
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Copy email command was ' + msg);
-
-        } catch(err) {
-
-            console.log('Oops, unable to copy');
-
-        }
-
-        // Remove the selections - NOTE: Should use
-        // removeRange(range) when it is supported
-        window.getSelection().removeAllRanges();
-
-
-    } else {
-        $shared = false;
-        $shareBtn.text('Share');
+chklogin();
+//--------------------------------------------------------------------------
+async function chklogin(){
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        const resFromProfile = await fetch('/user/profile', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const jsonFromProfile = await checkStatus(resFromProfile);
+        // const user = jsonFromProfile.data;
+        // userProfile(user);
+        let signin=document.getElementById('signin');
+        signin.setAttribute("style","display:none");
+        let signout=document.getElementById('signout');
+        signout.setAttribute("style","display:true");
     }
 }
-
-/**
- * removes the active class after a set period of time
- */
-function fadeOutNotification(){
-    setTimeout(function(){
-        $notificationButton.removeClass('active');
-    }, 2000);
-}
-
-// bind events
-$shareBtn.on('click', shareLink);
-$notificationButton.on('transitionend', fadeOutNotification);
